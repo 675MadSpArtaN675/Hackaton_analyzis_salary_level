1b0230048627951e26c23e8acee1b05a13b3fff2
675MadSpArtaN675 <va44l@yandex.ru>
2025-09-14 21:28:15 +0300

    Большинство модулей готовы

--- datatable.py
+++ datatable.py
@@ -1,17 +1,22 @@
 from dotenv import find_dotenv, load_dotenv
+from pprint import pprint, pp
 
 from month_data_parser import MoneyDataParser, MoneyDataFilesDownloader
 
-import os, io
+import os, re
 
 import asyncio as aio
 import pandas as pd
 import pdfminer.high_level as pmhl
 import pdfminer.layout as pml
 
-class TableCreator:
+class PdfStatisticsParser:
     __money_parser: MoneyDataParser
     __files_downloader: MoneyDataFilesDownloader
+
+    @staticmethod
+    def GetMonthName(filename: str):
+        return re.search(r"области\s(.*?)\s*\d{4}\s*года\.pdf$", filename).group(1)
 
     def __init__(self, url: str):
         load_dotenv(find_dotenv())
@@ -19,35 +24,77 @@ class TableCreator:
         self.__money_parser = MoneyDataParser(url)
         self.__files_downloader = MoneyDataFilesDownloader()
 
-    def Parser(self, year :int | str):
+    def ParseFiles(self, year :int | str):
+        files_start = "./files_downloaded"
+
+        if (not os.path.exists(files_start)):
+            os.mkdir(files_start)
+
+        sorted_years = self.__get_files_names(year)
+        self.__files_downloader.DownloadFiles(sorted_years)
+
+        files_data = pd.DataFrame(self.__get_data_from_files(files_start))
+        files_data.to_excel(excel_writer="./salaries.xlsx", 
+                  sheet_name="Уровень зарплат за текущий год")
+        
+        return files_data
+
+    def __get_files_names(self, year: int):
         docs = self.__money_parser.GetDocLinks(os.environ["TITLE_TO_FOUND"])
-        filtered = self.__money_parser.FilterByYear(docs, 2024)
+        filtered = self.__money_parser.FilterByYear(docs, year)
         sorted_years = self.__money_parser.MonthSort(filtered)
-        downloaded_files = self.__files_downloader.DownloadFiles(sorted_years)
 
-        files_data = []
-        for file in downloaded_files:
-            files_data.append(self.__parse_file_page(file))
+        return sorted_years
 
+    def __get_data_from_files(self, files_start):
+        files_data = dict()
+        for file in os.listdir(files_start):
+            filename = files_start + "/" + file
+
+            name = PdfStatisticsParser.GetMonthName(file)
+            lines = self.__parse_file_page(filename, 7)
+
+            files_data[name] = dict(lines)
+            os.remove(filename)
+
+        os.removedirs(files_start)
         return files_data
     
-    def __parse_file_page(self, file_data_: io.BytesIO):
-        print(file_data_)
-        file_data = io.BytesIO(file_data_)
-        output_fp = io.StringIO()
-
-        try:
-            pmhl.extract_text_to_fp(file_data, output_fp, page_numbers=[7])
-            
-            return output_fp
+    def __parse_file_page(self, file_name: str, number_page: int):
+        lines = []
+        for page in pmhl.extract_pages(file_name, page_numbers=[number_page - 1]):
+            for element in page:
+                if isinstance(element, pml.LTTextContainer):
+                    lines.extend([sub_element for sub_element in element])
         
-        except pmhl.pdfparser.PDFSyntaxError:
-            return None
+        return self.__filter_files(lines[3:])
+
+    def __filter_files(self, lines: list[pml.LTTextLine]):
+        cities = []
+        nums = []
+
+        for line in lines:
+            line_str = line.get_text().strip()
+
+            if line_str == "":
+                continue
+
+            if re.match(r"г\..*|.+Федерация|.+область", line_str):
+                cities.append(line_str)
+
+            elif re.match(r"\d+[^/\\]\s*\d+", line_str):
+                nums.append(line_str.replace(" ", ""))
+
+        sorted_nums = list(sorted(map(int, nums[:len(cities)]), reverse=True))
+
+        return zip(cities, sorted_nums)
+
 
 
 if __name__ == "__main__":
     url = "https://36.rosstat.gov.ru/folder/26390"
 
-    tc = TableCreator(url)
-    for i in tc.Parser(2024):
-        print(i.read())
+    psp = PdfStatisticsParser(url)
+    data = psp.ParseFiles(2025)
+    
+    
--- gigachat_service.py
+++ gigachat_service.py
@@ -1,6 +1,7 @@
 from gigachat import GigaChat
 from gigachat.models import Messages, MessagesRole, Chat
 
+import os
 
 class GigaChat_Service:
     __service: GigaChat
@@ -13,22 +14,28 @@ class GigaChat_Service:
     def send_message(self, message: str):
         message = Messages(content=message, role=MessagesRole.USER)
 
-        self.__send_message__(message)
+        return self.__send_message__(message)
 
     def send_message_with_file(self, message: str, file_ids: list[str]):
         message = Messages(content=message, role=MessagesRole.USER, attachments=file_ids)
         
-        self.__send_message__(message)
+        return self.__send_message__(message)
         
     def __send_message__(self, message: Messages):
         neuro_answer = self.__service.chat(self.__history)
         
         self.__history.append(message)
         self.__history.messages.extend(map(lambda x: x.message, neuro_answer.choices))
+        
+        return neuro_answer
     
     def upload_file_from_disk(self, file_path: str):
+        uploaded_file = None
         with open(file_path, "rb") as file:
-            return self.__service.upload_file(file)
+            uploaded_file = self.__service.upload_file(file)
+        
+        os.remove(file_path)
+        return uploaded_file
         
     def reset_chat_history(self, start_message: str):
         self.__history.messages.clear()
