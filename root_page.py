1bb8254c8c8c0f2b4bfa36d8ec116cb30dc25369
675MadSpArtaN675 <va44l@yandex.ru>
2025-09-16 16:14:05 +0300

    Доработка сервиса нейронки

--- root_page.py
+++ root_page.py
@@ -108,31 +108,3 @@ class RootPage(f.View):
         
         self.city_dataFrame.columns = column_indexes
         self.city_dataFrame.rows = [f.DataRow(city_data)]
-
-if __name__ == "__main__":
-    load_dotenv(find_dotenv())
-
-    sa = SalaryAnalyzer(os.getenv("LINK_TO_STATISTIC"), 
-                            START_MESSAGE, 
-                            os.getenv("GIGACHAT_TOKEN"))
-    
-    filename = sa.DownloadFilesFromWebSite()
-
-    def main(page: f.Page):
-        page.title = "Анализ уровня заработной платы"
-        page.theme_mode = f.ThemeMode.LIGHT
-
-        page.adaptive = True
-        page.vertical_alignment = f.MainAxisAlignment.START
-        page.horizontal_alignment = f.CrossAxisAlignment.CENTER
-
-        page.window.width = 900
-        page.window.height = 600
-
-        root = RootPage()
-        root.SetupUI(sa, filename)
-        
-        page.views.append(root)
-        page.update()
-
-    f.app(main)
